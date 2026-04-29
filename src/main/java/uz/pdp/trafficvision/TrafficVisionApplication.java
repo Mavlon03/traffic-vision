38d2d3f6d4997eb8e384d78131cd8a7815c91571
Akmalov Mavlon <180403589+Mavlon03@users.noreply.github.com>
2026-04-29 11:20:22 +0500

    First Commit

---
+++ src/main/java/uz/pdp/trafficvision/TrafficVisionApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.trafficvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficVisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/trafficvision/TrafficVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.trafficvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficVisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
